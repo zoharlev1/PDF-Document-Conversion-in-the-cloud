85b5d4b31520ef824ccf3c02268676dbb24a011a
Zohar <58083914+zoharlev1@users.noreply.github.com>
2022-08-22 13:42:23 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-import java.io.IOException;
-
-class main {
-    public static void main(String[] args) throws InterruptedException, IOException {
-        LocalApplication.main(new String []{args[0],args[1],args[2],args[3]});
-    }
-}
